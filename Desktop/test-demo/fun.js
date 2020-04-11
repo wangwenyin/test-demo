ee651f7d2ead77708721ce6b3b8e8b8c26cf0417
logo <logo>
2020-04-11 12:22:26 +0800

    'add'

--- Desktop/test-demo/fun.js
+++ Desktop/test-demo/fun.js
@@ -18,7 +18,7 @@ let a = (arr) => arr.slice().sort(() => Math.random() - 0.5)
 let b = a([1,2,3,4,5])
 console.log(b)
 
-// 5. 生成随机十六进制代码 如：'#c618b2'
+// 5. 生成随机十六进制颜色 码 如：'#c618b2'
 console.log('#' + Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, '0'))
 
 // 6. 这是一个臭名昭著的面试题，让你写出他的运行结果
@@ -28,3 +28,22 @@ for(i=0;++i<101;console.log(i%5?f||i:f+'Buzz'))f=i%3?'':'Fizz'
 const c= (_=>[..."`1234567890-=~~QWERTYUIOP[]\\~ASDFGHJKL;'~~ZXCVBNM,./~"].map(x=>(o+=`/${b='_'.repeat(w=x<y?2:' 667699'[x=["BS","TAB","CAPS","ENTER"][p++]||'SHIFT',p])}\\|`,m+=y+(x+'    ').slice(0,w)+y+y,n+=y+b+y+y,l+=' __'+b)[73]&&(k.push(l,m,n,o),l='',m=n=o=y),m=n=o=y='|',p=l=k=[])&&k.join`
 `)();
 console.log(c)
+
+// 8.生成长度为10的随机字母数字字符串
+Math.random().toString(36).substring(2);
+
+// 9.优雅的金钱格式化
+// 1、使用正则实现
+var test1 = '1234567890'
+var format = test1.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+console.log(format) // 1,234,567,890
+// 2、使用骚操作
+function formatCash(str) {
+       return str.split('').reverse().reduce((prev, next, index) => {
+            return ((index % 3) ? next : (next + ',')) + prev
+       })
+}
+console.log(format) // 1,234,567,890
+
+// 10. console美化
+console.info("%c哈哈", "color: #3190e8; font-size: 30px; font-family: sans-serif");
